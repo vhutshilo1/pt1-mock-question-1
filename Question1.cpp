8da25376f9134f6adb716558c049545f216ac823
John Poole <poolej@ukzn.ac.za>
2015-04-13 09:07:02 +0200

    add question 1

---
+++ Question1.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
+// Name:
+// Student No:
+// Date: 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// ***** QUESTION 1
+// Question 1 consists of 5 parts (order and approach is critical)
+
+int main(int argc, char *argv[]) {
+
+	// 1.1 on branch "master", uncomment the following line and make a commit
+	// cout << "This is the first line of code" << endl;
+
+	// 1.2 create and switch to branch "hello", uncomment (and complete) the following line and make a commit  
+	// cout << "Hello, my name is ___________." << endl;
+
+	// 1.3 switch to the "master" branch, uncomment the following four lines and make a commit
+	// string name;
+	// cout << "Please enter your name: ";
+	// cin >> name;
+	// cout << "Hello, " << name << endl;
+
+	// 1.4 merge branch "hello" into "master" keeping ALL of the code added so far
+
+	// 1.5 make a merge commit 
+
+}
